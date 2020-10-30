414d88c7bf25abb6d03dbeff280286a7f6ae6304
ThanhLam <hothanhlam197@gmail.com>
2020-10-30 18:17:36 +0700

    comit template 30/10/2020

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbAlertModule, NgbModule, NgbPaginationModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { SingleProductComponent } from './single-product/single-product.component';
+import { ListProductComponent } from './list-product/list-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingleProductComponent,
+    ListProductComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    NgbPaginationModule,
+    NgbAlertModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
